5dc1211055402ff8291a123e92d07b2bd4d7bb2f
demonzyz <13204854@qq.com>
2017-11-15 18:09:30 +0800

    Add_Event接口的注册

--- api/models.py
+++ api/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+
+class Add_Event(models.Model):
+    pass
--- demo/urls.py
+++ demo/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^hello/', include('hello.urls')),
+    url(r'^api/', include('api.urls')),
 ]
